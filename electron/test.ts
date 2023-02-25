f172b8c08f306ae2bd8889f2ca0fa8573c736811
shaonhuang <yukuaihuang@icloud.com>
2023-02-25 14:04:56 +0800

    fix running status

--- electron/test.ts
+++ electron/test.ts
@@ -1,18 +1,17 @@
-import Store from 'electron-store'
-
-const store = new Store();
-
-store.set('unicorn', '🦄');
-console.log(store.get('unicorn'));
-//=> '🦄'
-
-// Use dot-notation to access nested properties
-store.set('foo.bar', true);
-console.log(store.get('foo'));
-//=> {bar: true}
-
-store.delete('unicorn');
-console.log(store.get('unicorn'));
-//=> undefined
+// import Store from 'electron-store'
 //
-
+// const store = new Store();
+//
+// store.set('unicorn', '🦄');
+// console.log(store.get('unicorn'));
+// //=> '🦄'
+//
+// // Use dot-notation to access nested properties
+// store.set('foo.bar', true);
+// console.log(store.get('foo'));
+// //=> {bar: true}
+//
+// store.delete('unicorn');
+// console.log(store.get('unicorn'));
+// //=> undefined
+// //
--- src/pages/Config.tsx
+++ src/pages/Config.tsx
@@ -15,8 +15,8 @@ function Example() {
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us within 90 days and
-                we'll refund you in full, no questions asked.
+                If you re unhappy with your purchase for any reason, email us within 90 days and wel
+                l refund you in full, no questions asked.
               </Disclosure.Panel>
             </>
           )}
